111d88f5e4b4fe3a9e45baccd7f2a354d93d7e64
Seth Walton <seth.walton@ticketsforless.com>
2021-08-27 17:18:29 -0500

    Api built on Static Data

---
+++ MusicApiCourse/Controllers/SongsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicApiCourse.Models;
+
+namespace MusicApiCourse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song(){Id = 0, Title = "Willow", Language = "English"},
+            new Song(){Id = 1, Title = "Good Day Sunshine", Language = "English"},
+            new Song(){Id = 2, Title = "Happy Birthday", Language = "English"}
+        };
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Song song)
+        {
+            songs.Add(song);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Song song)
+        {
+            songs[id] = song;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
+    }
+}
